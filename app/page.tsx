e6fb5090163ffa58a4a6930432d348a2ff595d63
dhwani jain <dhwanijain03121606@gmail.com>
2026-06-04 16:33:28 +0530

    changed metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import LandingPage from "./components/LandingPage";
 import LaunchingSoon from "./components/LaunchingSoon";
 
 export const metadata: Metadata = {
-  title: "NevUp Built for clear decisions in noisy markets.",
+  title: "NevUp : Built for clear decisions in noisy markets.",
   description:
     "A behavioral intelligence layer for high-noise modern markets. Designed for the generation that won't accept a trading environment built against them.",
+    icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function Home() {
